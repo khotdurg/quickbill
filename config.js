5d1838bccb9ee9270ae7bf35555db4bf60fc1d96
Durgesh Khot <149609451+khotdurg@users.noreply.github.com>
2026-06-23 13:57:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,5 +10,6 @@ const GOOGLE_CLIENT_ID = "124931782063-08q4b448s4d53q3o60nookkra7uda1pl.apps.goo
 
 // Name of the folder this app creates inside each user's "My Drive"
 const DRIVE_FOLDER_NAME = "QuickBill Data";
+
 // Name of the database file inside that folder
 const DRIVE_FILE_NAME = "quickbill-database.json";
